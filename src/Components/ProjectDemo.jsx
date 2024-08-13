5eea0d2c9c4f137fd2ce843a108211616e5b162a
Aartisushmesh29 <134151553+Aartisushmesh29@users.noreply.github.com>
2024-08-13 18:12:32 +0530

    Updated Projects

--- src/Components/ProjectDemo.jsx
+++ src/Components/ProjectDemo.jsx
@@ -1,72 +1,64 @@
-import React from 'react';
-import BlogImage from '../assets/image-1.jpg'; // Make sure to replace this path with the actual path to your image
+import React from "react";
 
-
-const ProjectDemo = ({image , content , ProjectName}) => {
+const ProjectDemo = ({ img, title, skills, live_link, code_link }) => {
   return (
-    <div className='h-screen flex items-center justify-center  '>
-
-    <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-      <a href="#">
-        <img className="rounded-t-lg" src={image} alt="Blog" />
-      </a>
-      <div className="p-5">
-        <a href="#">
-          <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-            {ProjectName}
-          </h5>
-        </a>
-        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-         {content}
+    <div className="max-w-sm border border-gray-700 rounded-[10px] shadow dark:bg-gray-800 dark:border-gray-700 ">
+      <img src={img} alt="" className="object-fill h-40 w-80 rounded-[10px] " />
+      <div className="p-2 flex flex-col justify-evenly">
+        <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-50 dark:text-white">
+          {title}
+        </h5>
+        <p className="mb-3 font-normal text-gray-400 dark:text-gray-400">
+          Made with : <span className="font-semibold"> {skills} </span>
         </p>
-      <div className='flex justify-between'>
-        <a
-          href="#"
-          className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-        >
-         <button>Live</button>
-          <svg
-            className="rtl:rotate-180 w-3.5 h-3.5 ms-2"
-            aria-hidden="true"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 14 10"
+        <div className="flex justify-between">
+          <a
+            href={live_link}
+            className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-gray-800 rounded-lg hover:bg-gray-700  dark:text-black dark:bg-gray-100 dark:hover:bg-gray-300"
+            target="_blank"
           >
-            <path
-              stroke="currentColor"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M1 5h12m0 0L9 1m4 4L9 9"
-            />
-          </svg>
-        </a>
-        <a
-          href=""
-          className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-        >
-         <button>Code</button>
-          <svg
-            className="rtl:rotate-180 w-3.5 h-3.5 ms-2"
-            aria-hidden="true"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 14 10"
+            Live Demo
+            <svg
+              className="rtl:rotate-180 w-3.5 h-3.5 ms-2"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 14 10"
+            >
+              <path
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M1 5h12m0 0L9 1m4 4L9 9"
+              />
+            </svg>
+          </a>
+          <a
+            href={code_link}
+            className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-gray-50  rounded-lg hover:underline  dark:text-white"
+            target="_blank"
           >
-            <path
-              stroke="currentColor"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M1 5h12m0 0L9 1m4 4L9 9"
-            />
-          </svg>
-        </a>
-        </div> 
+            Go to Code
+            <svg
+              className="rtl:rotate-180 w-3.5 h-3.5 ms-2"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 14 10"
+            >
+              <path
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M1 5h12m0 0L9 1m4 4L9 9"
+              />
+            </svg>
+          </a>
+        </div>
       </div>
-    </div>
     </div>
   );
 };
-
 export default ProjectDemo;
--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -1,56 +1,51 @@
-import React from 'react';
-import img1 from '../assets/PROJ1.png';
-import img2 from '../assets/PROJ2.png';
-import img3 from '../assets/PROJ3.png';
+
+import React from "react";
+import img1 from "../assets/image.png";
+import img2 from "../assets/PROJ2.png";
+import img3 from "../assets/PROJ3.png";
+import ProjectDemo from "./ProjectDemo.jsx";
 
 const Projects = () => {
-  const projects = [
-    {
-      id: 1,
-      src: img1,
-      link: ""
-    },
-    {
-      id: 2,
-      src: img2,
-      link: "https://image-search-app-eight-pi.vercel.app/"
-    },
-    {
-      id: 3,
-      src: img3,
-      link: "https://vercel.com/aartisushmesh29s-projects/image-search-app"
-    },
-    {
-      id: 4,
-      src: img2,
-      link: "https://vercel.com/aartisushmesh29s-projects/image-search-app"
-    },
-    {
-      id: 5,
-      src: img2,
-      link: "https://vercel.com/aartisushmesh29s-projects/image-search-app"
-    },
-  ];
-
   return (
-    <div id='project' className='flex h-auto pt-4 flex-col bg-gradient-to-b from-black to-gray-800 w-full text-white'>
-      <div className='max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full'>
-        <div className='pb-8'>
-          <p className='text-4xl font-bold border-b-4 border-gray-500 p-2 inline'>Projects</p>
-          <p className='py-6'>Check out some of my work right here</p>
+    <div
+      id="project"
+      className="flex h-auto pt-4 flex-col bg-gradient-to-b from-black to-gray-800 w-full text-white"
+    >
+      <div
+        className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center
+    w-full h-full"
+      >
+        <div className="pb-8">
+          <p
+            className="text-4xl font-bold border-b-4
+         border-gray-500 p-2 inline"
+          >
+            Projects
+          </p>
+          <p className="py-6">Check out some of my work right here</p>
         </div>
-        <div className='grid grid-cols-1 md:grid-cols-3 gap-8 text-center py-8 px-12 sm:px-0'>
-          {projects.map(({ id, src, link }) => (
-            <div key={id} className='shadow-md shadow-gray-600 rounded-lg'>
-              <img src={src} alt={`Project ${id}`} className='rounded-md hover:scale-105 duration-200' />
-              <div className='flex items-center justify-center'>
-                <a href={link} target="_blank" rel="noopener noreferrer">
-                  <button className='w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105'>Demo</button>
-                </a>
-                <button className='w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105'>Code</button>
-              </div>
-            </div>
-          ))}
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+          <ProjectDemo
+            img={img1}
+            title="Glam-Gait: Shoe Site"
+            skills=" ReactJs , Node.js , MongoDb , ExpressJS , TailwindCSS , HTML , JavaScript , Postman "
+            live_link="https://glamgait-shopping.vercel.app/"
+            code_link="https://github.com/Aartisushmesh29/GlamGait_Shopping_App"
+          />
+          <ProjectDemo
+            img={img2}
+            title="Image Search App"
+            skills=" ReactJs, TailwindCSS , HTML , JavaScript , Api Integration, Postman "
+            live_link="https://image-search-app-eight-pi.vercel.app/"
+            code_link="https://github.com/Aartisushmesh29/Projects/tree/main/ImageSearch"
+          />
+          <ProjectDemo
+            img={img3}
+            title="Amazon Clone Frontend"
+            skills="HTML, CSS"
+            live_link="https://amazon-eosin-tau.vercel.app/"
+            code_link="https://github.com/Aartisushmesh29/Projects/tree/main/Amazon"
+          />
         </div>
       </div>
     </div>
